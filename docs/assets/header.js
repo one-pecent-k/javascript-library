69c60b157ac65d2793e53c4da38f7cf6ebf66dc5
Ino Maru <45377300+inomaru@users.noreply.github.com>
2022-10-23 09:57:35 +0900

    Fix some

---
+++ docs/assets/header.js
@@ -0,0 +1,5 @@
+document.currentScript.parentNode.appendChild(el = document.createElement('div'));
+el.className = 'p-3';
+el.appendChild(el = document.createElement('a'));
+el.textContent = 'TOP';
+el.href = '/';
